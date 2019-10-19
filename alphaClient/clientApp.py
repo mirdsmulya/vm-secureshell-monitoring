7fbd6501c2cf31905b4ece33865634825ae58187
mirdsmulya <by_ms19@yahoo.com>
2019-10-19 17:18:15 +0700

    Add collectSshLog countSshAttempt and init methods

--- alphaClient/clientApp.py
+++ alphaClient/clientApp.py
@@ -1,22 +1,41 @@
 
-class clientApp():
+class clientApp(object):
 
     def __init__:
-        pass
+        self.previoustAttempt = previoustAttempt
+        self.commandCLI = commandCLI
     
-    def collectHostname():
-        pass
 
-    def collectSSHLog():
+    def collectHostname(self, previoustAttempt):
         pass
-    
-    def countSshAttempt():
-        pass
+        
 
-    def sendToAlphaServer():
+    def collectSSHLog(self):
+        os.system(commandCLI)
+        logAttempt = open('ssh-attempt-log.txt', 'r')
+        return logAttempt
+
+
+    def countSshAttempt(self, logAttempt):
+        actualAttempt = 0
+        for text in logAttempt:
+            actualttempt += 1
+
+        if actualAttempt == self.previoustAttempt:
+            return False
+
+        self.previoustAttempt = actualAttempt
+        return actualAttempt
+
+
+    def sendToAlphaServer(self):
         pass
 
 
 if __name__ == "__main__":
-    pass
+    import os
+    previoustAttempt = 0
+    commandCLI = "cat /var/log/auth.log | grep Accepted > ssh-attempt-log.txt"
+
+    
 
