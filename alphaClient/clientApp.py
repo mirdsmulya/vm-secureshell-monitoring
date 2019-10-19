e5150467a735b7d22b9d43cf98fe1a0f552e70e2
mirdsmulya <by_ms19@yahoo.com>
2019-10-19 14:45:46 +0700

    initial commit

---
+++ alphaClient/clientApp.py
@@ -0,0 +1,22 @@
+
+class clientApp():
+
+    def __init__:
+        pass
+    
+    def collectHostname():
+        pass
+
+    def collectSSHLog():
+        pass
+    
+    def countSshAttempt():
+        pass
+
+    def sendToAlphaServer():
+        pass
+
+
+if __name__ == "__main__":
+    pass
+
