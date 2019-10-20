7a39285ca55f01744060993320b6afcfa3e5be1c
mirdsmulya <by_ms19@yahoo.com>
2019-10-21 06:36:34 +0700

    Change command order for LC_ALL to second place

--- test.sh
+++ test.sh
@@ -3,15 +3,14 @@
 echo "*********** Get update ************"
 yes | sudo apt-get update
 
+echo "*********** Export LC_ALL=C ************"
+export LC_ALL=C
+
 echo "*********** Instaling python ************"
 yes | sudo apt-get install python
 
 echo "*********** Instaling python-pip ************"
 yes | sudo apt-get install python-pip
-
-echo "*********** Export LC_ALL=C ************"
-export LC_ALL=C
-
 
 echo "********** Intalling Pytest ************"
 yes | sudo pip install -U pytest
